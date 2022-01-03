3b1a7e41848c71517ba9012ac2b9f8268ab1b90e
HeaYeon-Won <73468962+HeaYeon-Won@users.noreply.github.com>
2022-01-03 17:46:06 +0900

    Add files via upload

---
+++ IOT_CONTEST/cardb.sql
@@ -0,0 +1,10 @@
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '1', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '1', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '1', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '0', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '0', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '0', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '0', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '0', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '0', '전화번호');
+INSERT INTO `cardb`.`information` (`carNumber`, `prove`, `phoneNum`) VALUES ('차량번호', '0', '전화번호');
