b3cb0aa91724d0ab90802ddf910639a175fced4b
HeaYeon-Won <73468962+HeaYeon-Won@users.noreply.github.com>
2022-01-06 17:35:20 +0900

    Update 떡볶이떡 자르기.py

--- Algorithm Study/dong/떡볶이떡 자르기.py
+++ Algorithm Study/dong/떡볶이떡 자르기.py
@@ -2,15 +2,18 @@
 손님이 왔을 때 요청한 총 길이가 M일 때 적어도 M만큼의 떡을 얻기 위해 절단기에 설정할 수 있는 높이의 최댓값을 구하는 프로그램
 """
 def solution(n, m, dduck):
-    dduck.sort()
-    start, end = 0, dduck[-1]
+    start, end = 0, max(dduck)
+    answer=0
     while start<=end:
         mid =(start+end)//2
-        result = sum([i-mid for i in dduck if (i-mid)>0])
-        if result>m: start=mid+1
-        elif result<m: end=mid-1
-        elif result==m: return mid
-
+        result = 0
+        for i in dduck:
+            if i>mid: result+=(i-mid)
+        if result<m: end=mid-1
+        else:
+            answer=mid
+            start=mid+1
+    return answer
 
 n, m = map(int, input().split())
 dduck=list(map(int, input().split()))
